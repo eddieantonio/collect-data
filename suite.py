872ef70cc325e264b59acadf0a2e0c9d311571fe
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-28 09:01:59 -0600

    Use tqdm.

--- suite.py
+++ suite.py
@@ -5,6 +5,8 @@ import random
 import logging
 from time import sleep
 
+from tqdm import tqdm
+
 from measurements import Measurements
 
 logging.basicConfig(level=logging.DEBUG)
@@ -17,15 +19,18 @@ measure.define_configuration('docker')
 
 measure.define_test('idle')
 
-with measure.run_test('native', 'idle') as test_run:
-    for i in range(60):
-        test_run += random.gauss(70.0, 0.9)
-        sleep(1)
+with tqdm(total=2) as progress:
+    with measure.run_test('native', 'idle') as test_run:
+        for i in range(60):
+            test_run += random.gauss(70.0, 0.9)
+            sleep(1)
+    progress.update(1)
 
-with measure.run_test('docker', 'idle') as test_run:
-    for i in range(60):
-        test_run += random.gauss(70.1, 0.9)
-        sleep(1)
+    with measure.run_test('docker', 'idle') as test_run:
+        for i in range(60):
+            test_run += random.gauss(70.1, 0.9)
+            sleep(1)
+    progress.update(1)
 
 for datum in measure.energy():
     print(datum)
