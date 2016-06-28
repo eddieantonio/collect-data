2bf302c61063cdad573b1bd582200c99e005cf6a
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-27 19:33:12 -0600

    Remove extraneous things.

--- database.py
+++ database.py
@@ -3,30 +3,14 @@
 import sqlite3
 import logging
 
-from collections import namedtuple
-
-
-memory = lambda: sqlite3.connect(':memory:')
-
-class Energy(namedtuple('Energy', 'id configuration test energy')):
-    def __str__(self):
-        return ("{configuration}/{test} #{id}: "
-                "{energy:.1f}J".format(**self._asdict()))
-
-
 class Connection():
     def __init__(self, conn=None, name=':memory:'):
         self.conn = conn if conn is not None else sqlite3.connect(name)
-        self.install_functions()
-        self.conn.row_factory = lambda cursor, row: Energy(*row)
 
     def source(self, name):
         with open(name) as sqlfile:
             self.conn.executescript(sqlfile.read())
         return self
-
-    def install_functions(self):
-        self.conn.create_aggregate('ksum', 1, KahanSum)
 
     def query(self):
         cursor = self.conn.cursor()
--- sample.sql
+++ sample.sql
@@ -5,15 +5,15 @@ INSERT INTO configuration(name, description) VALUES
 INSERT INTO test(name, description) VALUES
     ('idle', 'Have the server do nothing for 60 minutes');
 
-INSERT INTO run(configuration, test, started, ended) VALUES
-    ('native', 'idle', datetime('now', 'utc', '-2.0 seconds'), datetime('now', 'utc')),
-    ('docker', 'idle', datetime('now', 'utc', '-2.0 seconds'), datetime('now', 'utc'));
+INSERT INTO run(configuration, test, started) VALUES
+    ('native', 'idle', datetime('now', 'utc', '-2.0 seconds')),
+    ('docker', 'idle', datetime('now', 'utc', '-2.0 seconds'));
 
 INSERT INTO measurement(run, timestamp, power) VALUES
-    ((SELECT id from run WHERE configuration = 'native' LIMIT 1), datetime('now', '-2.0 seconds'), 90.0),
-    ((SELECT id from run WHERE configuration = 'native' LIMIT 1), datetime('now', '-1.0 second'), 89.9),
-    ((SELECT id from run WHERE configuration = 'native' LIMIT 1), datetime('now', 'utc'), 90.1),
+    ((SELECT id FROM run WHERE configuration = 'native' LIMIT 1), datetime('now', '-2.0 seconds'), 90.0),
+    ((SELECT id FROM run WHERE configuration = 'native' LIMIT 1), datetime('now', '-1.0 second'), 89.9),
+    ((SELECT id FROM run WHERE configuration = 'native' LIMIT 1), datetime('now', 'utc'), 90.1),
 
     ((SELECT id from run WHERE configuration = 'docker' LIMIT 1), datetime('now', '-2.0 seconds'), 90.1),
-    ((SELECT id from run WHERE configuration = 'docker' LIMIT 1), datetime('now', '-1.0 second'), 90.3),
-    ((SELECT id from run WHERE configuration = 'docker' LIMIT 1), datetime('now', 'utc'), 90.2);
+    ((SELECT id FROM run WHERE configuration = 'docker' LIMIT 1), datetime('now', '-1.0 second'), 90.3),
+    ((SELECT id FROM run WHERE configuration = 'docker' LIMIT 1), datetime('now', 'utc'), 90.2);
