6e98b3100cebcab9f24f75e507d5e54f8734cbd7
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-29 14:01:06 -0600

    Add command line options to fake-wattsup.

--- test/fake-wattsup.py
+++ test/fake-wattsup.py
@@ -10,10 +10,39 @@ Emulates the output of wattsup invoked as:
 import sys
 import random
 
+import argparse
+
 from time import sleep
 
-sleep(random.randint(2, 10))
-while True:
-    print("%.1f" % (random.gauss(48.1, 0.2),))
-    sys.stdout.flush()
-    sleep(1)
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u', '--mean', type=float, default=48.1)
+parser.add_argument('-s', '--std-dev', type=float, default=0.2)
+
+parser.add_argument('-r', '--missing-rate', type=float, default=0.04)
+
+parser.add_argument('-D', '--no-delay', dest='delay', action='store_false')
+
+parser.add_argument('-m', '--min-delay', type=int, default=2)
+parser.add_argument('-M', '--max-delay', type=int, default=10)
+
+args = parser.parse_args()
+# Inject arguments into global scope.
+globals().update(args._get_kwargs())
+del args, parser
+
+assert min_delay <= max_delay
+assert 0.0 <= missing_rate < 1.0
+
+try:
+    if delay:
+        sleep(random.randint(min_delay, max_delay))
+
+    while True:
+        if not missing_rate or random.uniform(0.0, 1.0) > missing_rate:
+            print("{:.1f}".format(random.gauss(mean, std_dev)))
+            sys.stdout.flush()
+        sleep(1)
+
+except KeyboardInterrupt:
+    exit(0)
