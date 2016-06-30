8bd2e4e9aa4c5b0d66fcd1ac5a8785d633fd5754
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-30 10:39:10 -0600

    Control for randomness in interpolation test.

--- test_energy_aggregation.py
+++ test_energy_aggregation.py
@@ -6,7 +6,6 @@ Tests that the energy aggregation works properly.
 """
 
 import random
-from math import sqrt
 
 from measurements.energy_aggregation import EnergyAggregation
 from measurements import utc_date
@@ -90,17 +89,29 @@ def test_energy():
 
 
 def test_energy_missing_samples():
+    """
+    Tests that missing samples are interpolated.
+    """
+
     agg = EnergyAggregation()
 
     avg_watts = 48.1
-    give_or_take = .2
     test_duration = 60  # seconds
 
+    # Control for the randomness of the data:
+    # By having no randomness, the samples will all be the same value,
+    # hence, if no samples are missing, the calculated energy should be equal
+    # to the naïve estimation of energy (power * time).
+    give_or_take = 0
+
+    # Chris Solinas found that about 4.0% of samples from the Watts Up? were
+    # missing. Hence, test that we can interpolate them.
     samples = fabricate_data(N(μ=avg_watts, σ=give_or_take),
                              duration=test_duration,
                              percent_missing=4.0)
 
-    assert test_duration - 4 <= len(samples) <= test_duration, (
+    # Expect anywhere between 55 to 60 samples.
+    assert test_duration - 5 <= len(samples) <= test_duration, (
         "Data did not have a reason amount of samples"
     )
 
@@ -109,9 +120,7 @@ def test_energy_missing_samples():
     value = agg.finalize()
 
     estimated_avg_energy = avg_watts * test_duration
-    # I don't really know what the estimated standard deviation should be
-    # but let's make it grow at most O(√(|seconds|))
-    estimated_sd = give_or_take * sqrt(test_duration)
+    estimated_sd = .5  # allow for one Joule of fudge space.
 
     assert value in N(μ=estimated_avg_energy, σ=estimated_sd), (
         "Estimated energy value unlikely to be from the true distribution"
