400f4512356d3f9fb76b808e95c94fdbfe830618
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-29 21:57:41 -0600

    Change example suite to measure 60s idle.

--- suite.py
+++ suite.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import logging
+from datetime import datetime
+
 from tqdm import trange
 
 from measurements import Measurements, WattsUp
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,17 +16,26 @@ measure = Measurements('/tmp/test')
 measure.define_configuration('native', 'Linux running on Calzone')
 measure.define_experiment('idle', 'Place no load no the computer')
 
+wattsup = WattsUp('./test/fake-wattsup.py')
+
 logger.info('Waiting for Watts Up?...')
-wattsup = WattsUp('./test/fake-wattsup.py').wait_until_ready()
+wattsup.wait_until_ready()
 logger.info('Watts Up? ready')
+
+
+def seconds_elapsed_since(time):
+    return (datetime.now() - time).total_seconds()
+
 
 # Repeat the test 60 times.
 for _ in trange(60):
     with wattsup, measure.run_test('native', 'idle') as test_run:
-        for i in range(60):
+        start = datetime.now()
+        while seconds_elapsed_since(start) < 60.0:
             watts, timestamp = wattsup.next_measurement()
             test_run.add_measurement(watts, timestamp)
 
+# Finished with the Wattsup.
 wattsup.close()
 
 # Print the statistics back.
