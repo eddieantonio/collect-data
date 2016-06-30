6509e48c06cbff0659a4f119e78221f9bab6d2d3
Eddie Antonio Santos <easantos@ualberta.ca>
2016-06-29 23:32:32 -0600

    Add previous step 'interpolation'.

--- measurements/energy_aggregation.py
+++ measurements/energy_aggregation.py
@@ -3,6 +3,7 @@
 import logging
 import math
 
+from itertools import islice
 from collections import namedtuple
 
 __all__ = ['EnergyAggregation']
@@ -43,9 +44,48 @@ class EnergyAggregation:
         return self
 
     def finalize(self):
-        self.measurements.sort(key=lambda s: s.timestamp)
+        interpoloate_missing_measurements(self.measurements)
         return math.fsum(sample.watts for sample in self.measurements)
 
     @classmethod
     def install(cls, connection, name='energy'):
         connection.create_aggregate(name, 2, cls)
+
+
+def interpoloate_missing_measurements(measurements):
+    measurements.sort(key=lambda s: s.timestamp)
+
+    interpolated = []
+
+    for first, second in pairs(measurements):
+        number_missing = missing_measurements(first, second)
+        if number_missing == 0:
+            continue
+
+        logger.debug('%d measurement(s) missing between %f and %f (∆%f)',
+                     number_missing,
+                     first.timestamp,
+                     second.timestamp,
+                     (second.timestamp - first.timestamp))
+
+        # Interpolation: simply copy the first measurement for as many missing
+        # measurements as are required.
+        for i in range(number_missing):
+            sample = Measurement(watts=first.watts,
+                                 timestamp=first.timestamp + 1000 * (i + 1))
+            interpolated.append(sample)
+
+    measurements.extend(interpolated)
+    return measurements
+
+
+def missing_measurements(first, second):
+    assert second.timestamp > first.timestamp
+    difference = (second.timestamp - first.timestamp) / 1000.0 # in seconds
+
+    assert round(difference) >= 1
+    return round(difference) - 1
+
+
+def pairs(iterable):
+    return zip(iterable, islice(iterable, 1, None, 1))
