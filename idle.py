6ba21318a05bd0cc72ac27b3076d0be5c7800e71
Thincrust Admin <admin@thincrust>
2016-07-08 16:08:55 +0000

    Make idle tests use common infrastructure.

--- idle.py
+++ idle.py
@@ -1,34 +1,15 @@
 #!/usr/bin/env python
 
-from time import sleep
-from tqdm import trange
-
-from measurements import Measurements, Experiment, WattsUp
-
-
-measure = Measurements('energy.sqlite')
-
-
-# Start the Watts Up?
-wattsup = WattsUp(executable='/home/pi/wattsup/wattsup', args=['ttyUSB0', 'watts'])
-wattsup.wait_until_ready()
-
+from common import *
 
 # Define an experiment:
 @Experiment
 def idle():
     "Place no load on the computer"
-    sleep(60)
-
-config = measure.define_configuration('bare', 'Linux running on Chili (only sshd).')
+    sleep(1)
 
 # Run the experiment 60 times.
-measure.run(idle,
-            configuration=config,
-            repetitions=60,
-            range=trange,
-            wattsup=wattsup)
-
+run('bare', idle)
 
 # Finished with the Watts Up?
 wattsup.close()
