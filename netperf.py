5782edfbc95d421fe41cb101181f60fc99ddc4fa
Thincrust Admin <admin@thincrust>
2016-08-16 18:21:59 +0000

    Add experiments.

--- netperf.py
+++ netperf.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 
+from sh import netperf
 from common import *
 
 # Define an experiment:
 @Experiment
-def idle():
-    "Place no load on the computer"
-    sleep(60 * 60)
+def netperf0():
+    "Run an initial netperf stress test. Real parameters to be determined."
+    netperf(H='chili')
 
 # Run the experiment 60 times.
-run('bare', idle)
+run('bare', netperf0)
 
 # Finished with the Watts Up?
 wattsup.close()
