08362687f5294d725c06e05ecc596fa2b7ae73e6
Eddie Antonio Santos <easantos@ualberta.ca>
2016-08-18 12:51:22 -0600

    Add Chris's tests to the available experiments.

--- experiments.py
+++ experiments.py
@@ -2,17 +2,42 @@
 # -*- coding: UTF-8 -*-
 
 from time import sleep
-#from sh import netperf
 
-from measurements import Experiment
+from measurements import Experiment, env
 
 
 @Experiment
 def idle():
-    "Place no load on the computer for one hour."
-    #sleep(60 * 60)
-    sleep(5)
+    "Place no load on the computer for ten minutes."
+    sleep(60 * 10)
 
 
-# TODO: Write Chris's tests
-# TODO: Write Wordpress stress test
+@Experiment
+def redis():
+    """
+    Runs the Redis benchmark. See Solinas 2015 §4.1 for the original
+    parameters.
+    """
+    from sh import redis_benchmark
+
+    assert 'REDIS_HOST' in env, "You forgot to define REDIS_HOST"
+    redis_benchmark(h=env.REDIS_HOST, c=50, r=50000)
+
+
+@Experiment
+def postgresql():
+    """
+    Place no load on the computer for one hour.
+    """
+    from sh import pgbench
+
+    assert 'POSTGRES_HOST' in env, "You forgot to define POSTGRES_HOST"
+    pgbench(host=env.POSTGRES_HOST, username=env.POSTGRES_USER or 'postgres',
+            clients=50)
+
+
+@Experiment
+def wordpress():
+    "Wordpress stress test"
+    # TODO: Write Wordpress stress test
+    raise NotImplementedError
