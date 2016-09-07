0261143af3eaa3455310c86b5a844954504e902f
Eddie Antonio Santos <easantos@ualberta.ca>
2016-09-07 17:46:43 -0600

    Fix concurrency bug the right way.

--- measurements/wattsup.py
+++ measurements/wattsup.py
@@ -208,6 +208,17 @@ class WattsUp:
     def __enter__(self):
         self.wait_until_ready()
 
+        logger.debug("Starting collection.")
+
+        # Do as many dummy reads as needed to discard any data waiting from
+        # the last test.
+        # Concurrency bugs are the worst...
+        while self._conn.poll():
+            logger.debug("Discarding dummy data from previous test")
+            self._recv()
+
+        logger.debug("Cleared the queue. Recording!")
+
         # Allow sending messages.
         self._send('send')
         return self
@@ -215,10 +226,6 @@ class WattsUp:
     def __exit__(self, *exception_info):
         # Stop sending messages
         self._send('stop_send')
-        # Do a dummy read to discard any data waiting. Concurrency bugs are
-        # the worst...
-        if self._conn.poll():
-            self._recv()
 
     def _send(self, message):
         return self._conn.send(message)
